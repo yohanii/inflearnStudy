7a9e05403f94fa379f5c4cf47293c59efbc1b5ed
yohanii <dygks924@gmail.com>
2023-11-20 10:27:25 +0900

    feat: MemberRepository 구현

--- jpa1/src/test/java/jpabook/jpashop/MemberRepositoryTest.java
+++ jpa1/src/test/java/jpabook/jpashop/MemberRepositoryTest.java
@@ -1,5 +1,7 @@
 package jpabook.jpashop;
 
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.repository.MemberRepository;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
